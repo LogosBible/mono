LogosBible/mono

commit 37c8dfdb9b064a34f7cc1414e95dcfd8c8a0f5e5

Fix parsing of invalid int* exponents. Fixes #4396. (cherry picked from commit 997c36839d3da095be070116da5a22518524bf14)

mcs/class/corlib/System/Int32.cs
@@ -278,15 +278,14 @@ namespace System {
 		internal static bool FindExponent (ref int pos, string s, ref int exponent, bool tryParse, ref Exception exc)
 		{
 				exponent = 0;
-				long exp = 0; // temp long value
 
-				int i = s.IndexOfAny(new char [] {'e', 'E'}, pos);
-				if (i < 0) {
+				if (s [pos] != 'e' && s[pos] != 'E') {
 					exc = null;
 					return false;
 				}
 
-				if (++i == s.Length) {
+				var i = pos + 1;
+				if (i == s.Length) {
 					exc = tryParse ? null : GetFormatException ();
 					return true;
 				}
@@ -302,6 +301,7 @@ namespace System {
 					return true;
 				}
 
+				long exp = 0; // temp long value
 				for (; i < s.Length; i++) {
 					if (!Char.IsDigit (s [i]))  {
 						exc = tryParse ? null : GetFormatException ();

mcs/class/corlib/Test/System/Int16Test.cs
@@ -221,6 +221,12 @@ public class Int16Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+		
+		try {
+			Int16.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

mcs/class/corlib/Test/System/Int32Test.cs
@@ -316,6 +316,12 @@ public class Int32Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+
+		try {
+			Int32.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

mcs/class/corlib/Test/System/Int64Test.cs
@@ -372,6 +372,12 @@ public class Int64Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+		
+		try {
+			long.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

mcs/class/corlib/Test/System/UInt16Test.cs
@@ -221,6 +221,12 @@ public class UInt16Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+		
+		try {
+			UInt16.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

mcs/class/corlib/Test/System/UInt32Test.cs
@@ -256,6 +256,12 @@ public class UInt32Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+		
+		try {
+			uint.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

mcs/class/corlib/Test/System/UInt64Test.cs
@@ -245,6 +245,12 @@ public class UInt64Test
 			Assert.Fail ("B#7");
 		} catch (OverflowException) {
 		}
+		
+		try {
+			ulong.Parse ("2 math e1", NumberStyles.AllowExponent);
+			Assert.Fail ("B#8");
+		} catch (FormatException) {
+		}
 	}
 
 	[Test]

commit 3f8f07ce55d5f2872ff09a55cfa984aeb0b3474e

Fixing IndexOutOfRangeException crash introduced in commit 37c8dfdb9b064a34f7cc1414e95dcfd8c8a0f5e5.

mcs/class/corlib/System/Int32.cs
@@ -279,7 +279,7 @@ namespace System {
 		{
 				exponent = 0;
 
-				if (s [pos] != 'e' && s[pos] != 'E') {
+				if (pos >= s.Length || (s [pos] != 'e' && s[pos] != 'E')) {
 					exc = null;
 					return false;
 				}