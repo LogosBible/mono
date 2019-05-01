5de27988c80969ed8bce8d316519147fb72106b3
Martin Potter <martin.potter@logos.com>
2019-05-01 13:48:30 -0700

    Additional safeguards for disposing DeflateStreamNative while in use on another thread LBS-41099

--- mcs/class/System/System.IO.Compression/DeflateStream.cs
+++ mcs/class/System/System.IO.Compression/DeflateStream.cs
@@ -378,10 +378,12 @@ namespace System.IO.Compression
 			if (disposing && !disposed) {
 				disposed = true;
 				GC.SuppressFinalize (this);
-			
+
+				SafeDeflateStreamHandle zz = z_stream;
+				if (zz != null)
+					zz.Dispose();
+
 				io_buffer = null;
-			
-				z_stream.Dispose();
 			}
 
 			if (data.IsAllocated) {
@@ -423,10 +425,14 @@ namespace System.IO.Compression
 			if (io_buffer == null)
 				io_buffer = new byte [BufferSize];
 
-			int count = Math.Min (length, io_buffer.Length);
-			int n = base_stream.Read (io_buffer, 0, count);
+			byte [] managed_buffer = io_buffer;
+			if (managed_buffer == null)
+				return 0;
+
+			int count = Math.Min (length, managed_buffer.Length);
+			int n = base_stream.Read (managed_buffer, 0, count);
 			if (n > 0)
-				Marshal.Copy (io_buffer, 0, buffer, n);
+				Marshal.Copy (managed_buffer, 0, buffer, n);
 
 			return n;
 		}
@@ -443,14 +449,18 @@ namespace System.IO.Compression
 
 		int UnmanagedWrite (IntPtr buffer, int length)
 		{
+			if (io_buffer == null)
+				io_buffer = new byte [BufferSize];
+
+			byte [] managed_buffer = io_buffer;
+			if (managed_buffer == null)
+				return 0;
+
 			int total = 0;
 			while (length > 0) {
-				if (io_buffer == null)
-					io_buffer = new byte [BufferSize];
-
-				int count = Math.Min (length, io_buffer.Length);
-				Marshal.Copy (buffer, io_buffer, 0, count);
-				base_stream.Write (io_buffer, 0, count);
+				int count = Math.Min (length, managed_buffer.Length);
+				Marshal.Copy (buffer, managed_buffer, 0, count);
+				base_stream.Write (managed_buffer, 0, count);
 				unsafe {
 					buffer = new IntPtr ((byte *) buffer.ToPointer () + count);
 				}
